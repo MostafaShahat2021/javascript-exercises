f610b37eef9ed595a67a4c455100962e45585aeb
MostafaShahat2021 <mostafa.shahat2014@gmail.com>
2024-07-30 16:37:03 +0300

    add console.log() statment to check git

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -3,6 +3,7 @@ const reverseString = function (str) {
   for (let i = str.length-1; i >= 0; i--) {
     reversed += str[i]
   }
+  console.log(reversed);
   return reversed
 };
 reverseString("hello")
